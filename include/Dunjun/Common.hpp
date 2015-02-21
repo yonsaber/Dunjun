bb52b03d297c5556b81e10d3de19663f5b66edbf
yonsaber <matt@mattredmond.co.uk>
2015-02-21 18:55:40 +0000

    Project Setup
    :octocat: Initial git commit

---
+++ include/Dunjun/Common.hpp
@@ -0,0 +1,12 @@
+/// Common code used in the Dunjun project
+#ifndef DUNJUN_COMMON_HPP
+#define DUNJUN_COMMON_HPP
+
+namespace Dunjun
+{
+using Float32 = float;  // Replace instances of Float32 with float
+using Float64 = double; // Replace instances of Float64 with double
+
+} // namespace Dunjun
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Dunjun/Common.hpp>
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	std::cout << "Hello World\n";
+
+	std::cin.ignore();
+
+	return 0;
+}
